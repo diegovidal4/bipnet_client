ad7ccb00c81fda01ee7f35e4a4f922051556c18a
Diego <diego.vidal4@gmail.com>
2014-12-15 19:10:48 +0000

    lcd integration

---
+++ display_keypad.py
@@ -0,0 +1,32 @@
+<<<<<<< HEAD
+=======
+import RPi.GPIO as GPIO
+>>>>>>> afaa1703da1c07f2c55114c0c1d1dbc21919dd67
+import lcd_kp as lib
+import time
+import os
+import sys
+from evdev import InputDevice
+
+
+#KeyPad a usar
+dev = InputDevice("/dev/input/by-id/usb-05d5_KEYBOARD-event-kbd")
+dev.grab() # mio!
+
+def main():
+
+  lib.setup()
+  lib.lcd_init()
+  
+  lib.lcd_goto(1,0)
+  
+  lib.lcd_string("Device found:")
+  lib.lcd_goto(2,1)  
+
+  print lib.kp_input(dev)
+
+  time.sleep(1)
+  lib.cleanup()
+
+if __name__ == '__main__':
+  main()

034e7b9ca63f50ee120b43334d2384daeb3c7179
diegovidal4 <diego.vidal4@gmail.com>
2014-12-14 23:41:52 -0300

    fix size pass

--- bipnet.py
+++ bipnet.py
@@ -1,8 +1,9 @@
-import utils
+from utils import Utils
 
 if __name__=="__main__":
+	tools=Utils()
 	#Se genera un string aleatorio de largo 6 para la password
-	password=utils.pass_generator()
+	password=tools.pass_generator(8)
 	print "La password es:%s" % password
 	#Se crea la variable con toda la configuracion a escribir en hostapd
 	hostapd_conf="interface=wlan0\nssid=BIPnet\nhw_mode=g\nchannel=6\nauth_algs=1\nwmm_enabled=0\nwpa=2\nwpa_passphrase=%s\nwpa_key_mgmt=WPA-PSK\nwpa_pairwise=TKIP\nrsn_pairwise=CCMP\n" % password
@@ -13,7 +14,7 @@ if __name__=="__main__":
 		conf_file.write(hostapd_conf)
 
 	try:
-		utils.hostapd()
-		utils.clock(120)
+		tools.hostapd()
+		tools.clock(120)
 	except:
 		print "Error: Hostapd no encontrado"
--- utils.py
+++ utils.py
@@ -3,23 +3,23 @@ import random
 import subprocess
 import sys,time
 
-def pass_generator(size=6,chars=string.ascii_lowercase + string.digits):
-	return ''.join(random.choice(chars) for _ in range(size))
+class Utils:
+    def pass_generator(self,size=6,chars=string.ascii_lowercase + string.digits):
+    	return ''.join(random.choice(chars) for _ in range(size))
 
-def clock(seconds):
-    if seconds > 60:
-        minutes=seconds/60
-        for remaining_min in range(minutes,0,-1):
-            for remaining_seg in range(60, 0, -1):
-                sys.stdout.write("\r")
-                sys.stdout.write("{:2d} minutes and {:2d} seconds remaining.".format(remaining_min,remaining_seg)) 
-                sys.stdout.flush()
-                time.sleep(1)
+    def clock(self,seconds):
+        if seconds > 60:
+            minutes=seconds/60
+            for remaining_min in range(minutes,0,-1):
+                for remaining_seg in range(60, 0, -1):
+                    sys.stdout.write("\r")
+                    sys.stdout.write("{:2d} minutes and {:2d} seconds remaining.".format(remaining_min,remaining_seg)) 
+                    sys.stdout.flush()
+                    time.sleep(1)
+        sys.stdout.write("\r")
+        sys.stdout.flush()    
+        sys.stdout.write("\rComplete!\n")
 
-    sys.stdout.write("\r")
-    sys.stdout.flush()    
-    sys.stdout.write("\rComplete!\n")
 
-
-def hostapd(command='status'):
-    subprocess.check_call(['service','hostapd',command])
+    def hostapd(self,command='status'):
+        subprocess.check_call(['service','hostapd',command])

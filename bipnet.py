388f8b11d15acec2c13a507205bbbe1a3d8f3043
diegovidal4 <diego.vidal4@gmail.com>
2014-12-16 13:24:47 -0300

    loop coupon

--- bipnet.py
+++ bipnet.py
@@ -46,6 +46,7 @@ if __name__=="__main__":
 			lcd.lcd_string("Tienes Cupon?")
 			lcd.lcd_goto(2,0)
 			lcd.lcd_string("1. Si, 2. No")
+			tiene_cupon=''
 			while tiene_cupon=='':
 				time.sleep(0.1)
 				tiene_cupon=lcd.kp_input(dev)
